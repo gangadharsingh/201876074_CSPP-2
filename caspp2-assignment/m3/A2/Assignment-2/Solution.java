d657051ab3db770aab18ebbbe2161b728c25c9b4
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-29 20:19:08 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ caspp2-assignment/m3/A2/Assignment-2/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+/**.
+    *@author: Gangadhar
+    *Date:29/08/18
+    *wnumber of 7's in between 1 to n.
+*/
+public final class Solution {
+	/** Fill the main function to print the number of 7's between 1 to n
+	*{using while and for loop}
+	*/
+	private Solution() {
+	}
+	/**.
+	*{main function}
+	*/
+	private static final int NUM = 7;
+    /**.
+    *creating NUM variable
+    */
+	/**.
+	*@param args taking argument
+	*/
+    public static void main(final String[] args) {
+    	/**.
+		*main function.
+		*/
+
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        for(int i = 1; i <= n; i++) {
+        	int k = i;
+        	while (k > 0) {
+        		if (k%10 == NUM) {
+        			count = count + 1;
+        		}
+        		k = k / 10;
+
+        	}
+
+        }  
+        System.out.println(count);
+    }
+}
