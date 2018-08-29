8f963620a6427d7ae4f0afbfa35ae8595cb5e66c
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-29 18:16:56 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- caspp2-assignment/m2/Assignment-2/Solution.java
+++ caspp2-assignment/m2/Assignment-2/Solution.java
@@ -4,34 +4,34 @@ import java.util.Scanner;
     *rootsOfQuadraticEquation function.
 */
 public final class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	private Solution() {
+    /*
+    Do not modify this main function.
+    */
+    private Solution() {
     }
     /**
-	*@param args taking argument
-	*/
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-	/**
-	*@param a int
-	*@param b int
-	*@param c int
-	*/
-	public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-		double n = (b * b) - (2 *2 * a * c);
-		double quad = Math.sqrt(n);
-		double root1 = (( c + quad) / (2 * a));
-		double root2 = (( c - quad) / (2 * a));
-		System.out.println(root1 + " " + root2);
-	}
+    *@param args taking argument
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    */
+    /**
+    *@param a int
+    *@param b int
+    *@param c int
+    */
+    public static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+        double n = (b * b) - (4 * a * c);
+        double quad = Math.sqrt(n);
+        double root1 = (( c + quad) / (2 * a));
+        double root2 = (( c - quad) / (2 * a));
+        System.out.println(root1 + " " + root2);
+    }
 }
