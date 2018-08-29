c7ae0e41875ee4fde6a0ea135ae01db8a0732066
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-29 20:19:10 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- caspp2-assignment/m2/Assignment-3/Solution.java
+++ caspp2-assignment/m2/Assignment-3/Solution.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
-
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+/**.
+    *@author: Gangadhar
+    *Date:28/08/18
+    *power function.
+*/
+public final class Solution {
+    /*
+    Do not modify this main function.
+    */
+    private Solution() {
+    }
+    /**.
+    *@param args is argument
+    */
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        int result=power(base,exponent);
+        long result=power(base,exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    /**
+    *Need to write the power function and print the output.
+    */
+    /**
+    *@param base base
+    *@param exp exponential
+    */
+    public static long power(final int base, final int exp) {
+        if (exp != 0) {
+            return (base * power(base, (exp - 1)));
+        } else {
+            return 1;
+        }
+    }
 }
