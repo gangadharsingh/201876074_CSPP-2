3bdd7fef9de0bdb1116da1b170742ea9ab0ebfd7
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-29 20:23:00 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- caspp2-assignment/m3/A2/Assignment-2/Solution.java
+++ caspp2-assignment/m3/A2/Assignment-2/Solution.java
@@ -5,40 +5,41 @@ import java.util.Scanner;
     *wnumber of 7's in between 1 to n.
 */
 public final class Solution {
-	/** Fill the main function to print the number of 7's between 1 to n
-	*{using while and for loop}
-	*/
-	private Solution() {
-	}
-	/**.
-	*{main function}
-	*/
-	private static final int NUM = 7;
+    /** Fill the main function to print the number of 7's between 1 to n.
+    *{using while and for loop}
+    */
+    private Solution() {
+    }
+    /**.
+    *{main function}
+    */
+    private static final int NUM = 7;
+    private static final int NUM2 = 7;
     /**.
     *creating NUM variable
     */
-	/**.
-	*@param args taking argument
-	*/
+    /**.
+    *@param args taking argument
+    */
     public static void main(final String[] args) {
-    	/**.
-		*main function.
-		*/
+        /**.
+        *main function.
+        */
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
-        for(int i = 1; i <= n; i++) {
-        	int k = i;
-        	while (k > 0) {
-        		if (k%10 == NUM) {
-        			count = count + 1;
-        		}
-        		k = k / 10;
+        for (int i = 1; i <= n; i++) {
+            int k = i;
+            while (k > 0) {
+                if (k % NUM2 == NUM) {
+                    count = count + 1;
+                }
+                k = k / NUM2;
 
-        	}
+            }
 
-        }  
+        }
         System.out.println(count);
     }
 }
