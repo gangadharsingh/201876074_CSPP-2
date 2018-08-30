daa797884cbe4681bdf87a8d26d088d4bcf1aa34
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-30 10:53:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- caspp2-assignment/m3/A3/Assignment-3/Solution.java
+++ caspp2-assignment/m3/A3/Assignment-3/Solution.java
@@ -18,21 +18,21 @@ public final class Solution {
     */
     public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s=new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         System.out.println(gcd(n1,n2));
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
+    Need to write the gcd function and print the output.
+    */
     /**
     *@param a variable
     *@param b variable
-    *@return long
+    *@return int
     */
     public static int gcd(final int a, final int b) {
-        if (a==0 || b==0) {
+        if (a == 0 || b == 0) {
             return 0;
         } 
         if (a == b) {
@@ -41,7 +41,7 @@ public final class Solution {
         if (a > b) {
             return gcd((a - b), b);
         } else {
-            return gcd(a, (b-a));
+            return gcd(a, (b - a));
         }
     }
 }
