05cb980972b35dd6c4cbb57867fea04eca5b581d
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-31 15:18:44 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- caspp2-assignment/m4/a3/Assignment-3/Solution.java
+++ caspp2-assignment/m4/a3/Assignment-3/Solution.java
@@ -9,7 +9,6 @@ public class Solution {
     public final static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
         for (int i = 0; i <= n; i++) {
             String s = sc.nextLine();
             String res = binaryToDecimal(s);//Write binaryToDecimal function
