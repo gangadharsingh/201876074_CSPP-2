ea96df8458fac9f2576963c8b9967d5f1a5185c6
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-31 14:48:49 +0530

    "check_style_score -> 0 of 1 passed. style score: 0/1 "

--- caspp2-assignment/m4/a3/Assignment-3/Solution.java
+++ caspp2-assignment/m4/a3/Assignment-3/Solution.java
@@ -3,31 +3,31 @@
 
 
 import java.util.Scanner;
-public class Solution
-{/*
-    Do not modify this main function.
-    */
-    public final static void main(final String[] args)
-    {
+public class Solution {
+    /*
+       Do not modify this main function.
+       */
+    public final static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i <= n; i++){
+        for (int i = 0; i <= n; i++) {
             String s = sc.nextLine();
             String res = binaryToDecimal(s);//Write binaryToDecimal function
             System.out.println(res);
         }
     }
-    
-  public static String binaryToDecimal(String s) {  
-    char[] arr = s.toCharArray();
-    int num = 0;
-    String l = "";
-    for (int i = arr.length; i > 1; i--) {
-        if (arr[i] == '1') {
-            num += Math.pow(2, i);
+
+    public static String binaryToDecimal(String s) {
+        char[] arr = s.toCharArray();
+        int num = 0;
+        int cnt = 0;
+        String l = "";
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == '1') {
+                num += Math.pow(2, i);
+            }
         }
+        l += num;
+        return l;
     }
-    l += num;
-    return l;
-  }
 }
