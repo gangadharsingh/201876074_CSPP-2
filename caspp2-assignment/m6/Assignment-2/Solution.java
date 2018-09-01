700ef247df5b5ec5c230b5d2d92d2ed6b097a162
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-01 10:42:46 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- caspp2-assignment/m6/Assignment-2/Solution.java
+++ caspp2-assignment/m6/Assignment-2/Solution.java
@@ -9,34 +9,37 @@ final class Solution {
     /**
      * Constructs the object.
      */
+    public static final int NUM1 = 100;
+    public static final int NUM2 = 50;
+    public static final int NUM3 = 10;
+    public static final int NUM4 = 5;
     private Solution() {
         //not used
     }
     /**
-     * Function to round the
-     * elements of a matrix to the nearest 100.
+     * Function to round the elements of a matrix to the nearest 100.
      *
-     * @param      a     Matrix of which the elements to be rounded
+     * @param      a        Matrix of which the elements to be rounded
      * @param      rows     Number of rows
-     * @param      columns     Number of columns
+     * @param      columns  Number of columns
      *
      * @return     Matrix of the rounded elements
      */
-    static int[][] roundHundred(final int[][] a, 
+    static int[][] roundHundred(final int[][] a,
         final int rows, final int columns) {
 
     // write ypur code here
     int[][] round = new int[rows][columns];
     for (int i = 0; i < a.length; i++) {
         for (int j = 0; j < a[0].length; j++) {
-            if (a[i][j] / 100 > 0) {
-                if (a[i][j] % 100 >= 50) {
-                    round[i][j] = ((a[i][j] / 100) + 1) * 100;
+            if (a[i][j] / NUM1 > 0) {
+                if (a[i][j] % NUM1 >= NUM2) {
+                    round[i][j] = ((a[i][j] / NUM1) + 1) * NUM1;
                 } else {
-                    round[i][j] = (a[i][j] / 100) * 100;
+                    round[i][j] = (a[i][j] / NUM1) * NUM1;
                 }
-            } else if (a[i][j] / 10 >= 5) {
-                round[i][j] = 100;
+            } else if (a[i][j] / NUM3 >= NUM4) {
+                round[i][j] = NUM1;
             } else {
                 round[i][j] = 0;
             }
