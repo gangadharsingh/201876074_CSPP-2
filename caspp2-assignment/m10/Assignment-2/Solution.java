3867483b03ac2dc4b451e0b4647e716bbb1063fc
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-06 17:35:30 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- caspp2-assignment/m10/Assignment-2/Solution.java
+++ caspp2-assignment/m10/Assignment-2/Solution.java
@@ -6,6 +6,14 @@ import java.util.Scanner;
  */
 class Solution {
     /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * empty constructor.
+         */
+    }
+    /**
      * {main function}.
      *
      * @param      args  The arguments
