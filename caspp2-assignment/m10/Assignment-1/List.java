2428c02d155caab673abac3ad925b8385b86315e
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-06 14:09:24 +0530

    "check_style_score -> 2 of 3 passed. style score: 1/1 "

--- caspp2-assignment/m10/Assignment-1/List.java
+++ caspp2-assignment/m10/Assignment-1/List.java
@@ -296,7 +296,7 @@ public class List {
      * Inserts all the elements of specified int array to the end of list.
      * @param  items items are list of items.
      */
-    public void addAll (final int[] items) {
+    public void addAll(final int[] items) {
         // write the logic
         int cnt = 0;
         if (items.length - 1 > list.length - 1) {
