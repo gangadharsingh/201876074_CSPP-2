841bd2095980a17d14eca32bbcf5e9212556bac7
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-06 10:44:53 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- caspp2-assignment/m9/Assignment-2/HW/Homework /List.java
+++ caspp2-assignment/m9/Assignment-2/HW/Homework /List.java
@@ -298,10 +298,12 @@ public class List {
         */
     public void add(int index, int item) {
         // write the logic
-        for (int i = size; i > index; i--) {
+        for (int i = list.length - 2; i >= index; i--) {
             list[i+1] = list[i];
+            if (i == index) {
+                list[i] = item;
+            }
         }
-        list[index] = item;  
     }
 
     /* Returns the count of occurances of a given item in the list*/
