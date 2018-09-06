f14523a41d7ca922df5ed45c9e3fdf9d716b6365
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-06 12:33:12 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- caspp2-assignment/m10/Assignment-1/List.java
+++ caspp2-assignment/m10/Assignment-1/List.java
@@ -317,7 +317,7 @@ public class List {
         } else if (index < 0){
             System.out.println("Negative Index Exception");
         } else if (index == 0 && size == 0 && item == 0) {
-            list[0] = 999; //to control special
+            list[0] = 0; //to control special
                            //case of index and item both are 0
         }
     }
