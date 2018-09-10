3e487d5e492bf47e3e92a780ed7381d0a9db18d0
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-10 21:45:50 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- caspp2-assignment/m14/SortedSet (1)/SortedSet/Solution.java
+++ caspp2-assignment/m14/SortedSet (1)/SortedSet/Solution.java
@@ -1,9 +1,27 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ *@author : gangadharsingh.
+ * Class for solution.
+ */
 public class Solution {
-
-	public static int[] intArray(final String s) {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        /**
+         * empty constructor.
+         */
+    }
+    /**
+     * creating Solution function.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int[] intArray(final String s) {
         String input = s;
         if (input.equals("[]")) {
             return new int[0];
@@ -16,8 +34,13 @@ public class Solution {
                .toArray();
     }
 
-	public static void main(String[] args) {
-		SortedSet s = new SortedSet();
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        SortedSet s = new SortedSet();
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
@@ -26,14 +49,13 @@ public class Solution {
             String line = stdin.nextLine();
             // split the line using space
             String[] tokens = line.split(" ");
-            //System.out.println(tokens[0]+"tokens[0]");
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
             case "last":
                 System.out.println(s.last());
                 break;
             case "addAll":
-            	int[] intArray = intArray(tokens[1]);
+                int[] intArray = intArray(tokens[1]);
                 s.addAll(intArray);
                 break;
             case "print":
@@ -46,70 +68,97 @@ public class Solution {
                 }
                 break;
             case "subSet":
-				/*intArray = intArray(tokens[0]);
-                intArray = intArray(tokens[1]);*/
                 String[] t = tokens[1].split(",");
                 int[] n = s.subSet(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
                 if (n.length != 1) {
-                	System.out.println(Arrays.toString(s.subSet(Integer.parseInt(t[0]), Integer.parseInt(t[1]))).replace("[", "{").replace("]", "}"));
+                    System.out.println(Arrays.toString(s.subSet(Integer.parseInt(t[0]), Integer.parseInt(t[1]))).replace("[", "{").replace("]", "}"));
                 }
                 break;
-			}
-		}
-	}
+                default:
+            }
+        }
+    }
 }
 class SortedSet extends Set {
 
-	public int[] subSet(int fromElement, int toElement) {
-		if (fromElement > toElement) {
-			System.out.println("Invalid Arguments to Subset Exception");
-			return new int[]{-1};
-		}
-		int[] temp = new int[size];
-		int count = 0;
-		for (int i = 0; i < size; i++) {
-			if (set[i] >= fromElement && set[i] < toElement) {
-				temp[count++] = set[i];
-			}
-		}
-		int[] subset = new int[count];
-		for (int i = 0; i < count;i++ ) {
-			subset[i] = temp[i];
-		}
-		return subset;
-	}
-	public int[] headSet(int toElement) {
-		int count = 0;
-		int[] temp = new int[size];
-		for (int i = 0; i < size; i++) {
-			if (set[i] < toElement) {
-				temp[count++] = set[i];
-			}
-		}
-		int[] headset = new int[count];
-		for (int i = 0; i < count ;i++ ) {
-			headset[i] = temp[i];
-		}
-		return headset;
-	}
-	public int last() {
-		if (size == 0) {
-			System.out.println("Set Empty Exception");
-			return -1;
-		} else {
-			return set[size -1];
-		}
-	}
-	public void addAll(int[] array) {
-		for (int i = 0; i < array.length; i++) {
+    /**
+     * return subset.
+     *
+     * @param      fromElement  The from element.
+     * @param      toElement    To element.
+     *
+     * @return     { return integer array }.
+     */
+    public int[] subSet(int fromElement, int toElement) {
+        if (fromElement > toElement) {
+            System.out.println("Invalid Arguments to Subset Exception");
+            return new int[] { -1};
+        }
+        int[] temp = new int[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (set[i] >= fromElement && set[i] < toElement) {
+                temp[count++] = set[i];
+            }
+        }
+        int[] subset = new int[count];
+        for (int i = 0; i < count; i++ ) {
+            subset[i] = temp[i];
+        }
+        return subset;
+    }
+    /**
+     * return element upto toelement.
+     *
+     * @param      toElement  To element.
+     *
+     * @return     { int array }.
+     */
+    public int[] headSet(int toElement) {
+        int count = 0;
+        int[] temp = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (set[i] < toElement) {
+                temp[count++] = set[i];
+            }
+        }
+        int[] headset = new int[count];
+        for (int i = 0; i < count ; i++ ) {
+            headset[i] = temp[i];
+        }
+        return headset;
+    }
+    /**
+     * the funciton will return last element of sorted set.
+     * @return     { }
+     */
+    public int last() {
+        if (size == 0) {
+            System.out.println("Set Empty Exception");
+            return -1;
+        } else {
+            return set[size - 1];
+        }
+    }
+    /**
+     * Adds all.
+     *
+     * @param      array  The array
+     */
+    public void addAll(int[] array) {
+        for (int i = 0; i < array.length; i++) {
             add(array[i]);
         }
         set = Arrays.copyOf(set, size);
-		Arrays.sort(set);
-		// super.toString()
-	}
+        Arrays.sort(set);
+    }
 
-	public String toString() {
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
         if (size == 0) {
             return "{}";
         }
