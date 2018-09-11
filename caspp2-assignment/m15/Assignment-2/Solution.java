373f2f357c58d2fbb57a4cbe56c099e89c0bc3c2
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-12 01:38:05 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- caspp2-assignment/m15/Assignment-2/Solution.java
+++ caspp2-assignment/m15/Assignment-2/Solution.java
@@ -69,7 +69,7 @@ public final class Solution {
                     intArray = intArray(tokens[1]);
                     if (intArray.length == 1) {
                         System.out.println(Arrays.toString(s.headSet(
-                                                               intArray[0])).replace("[", "{").replace("]", "}"));
+                                        intArray[0])).replace("[", "{").replace("]", "}"));
                     }
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
@@ -97,8 +97,8 @@ public final class Solution {
                     int[] n = s.subSet(Integer.parseInt(
                                            t[0]), Integer.parseInt(t[1]));
                     if (n.length != 1) {
-                        System.out.println(Arrays.toString(
-                                               n).replace("[", "{").replace("]", "}"));
+                        System.out.println(Arrays.toString
+                            (n).replace("[", "{").replace("]", "}"));
                     }
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
