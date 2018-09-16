569e827d763e96177595b1bebe390b56fe0cc479
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-09-16 13:31:31 +0530

    "check_style_score -> 8 of 13 passed. style score: 0/1 "

--- caspp2-assignment/m19/Assignment-1/Solution.java
+++ caspp2-assignment/m19/Assignment-1/Solution.java
@@ -174,7 +174,7 @@ public final class Solution {
                     choiceans = quizset[i].getchoice();
                     for (int j = 0; j < choiceans.length; j++) {
                         if (choiceans[j].equals(useresponse[i])) {
-                            System.out.println(quizset[i].getquestiontext() + "\n" + " Correct Answer! - Marks Awarded: " + quizset[i].getmaxmark());
+                            System.out.println(quizset[j].getquestiontext() + "\n" + " Correct Answer! - Marks Awarded: " + quizset[j].getmaxmark());
                             totalscore += quizset[j].getmaxmark();
                         } /*else {
                             System.out.println(quizset[i].getquestiontext() + "\n" + " Wrong Answer! - Penalty: " + quizset[i].getpenalty());
